5effe3fb479298a520a21f1f27a09295a8939a17
Thomas Martin <161907136+TommyMart@users.noreply.github.com>
2024-09-01 12:27:25 +0930

    Drag and drop bug fix

--- src/diceDropper.js
+++ src/diceDropper.js
@@ -37,10 +37,10 @@ function drop(event){
 	// dice result will be displayed
 	let diceCopyText = diceCopy.querySelector("p");
 	// Rolls the dice
-	const rollValue = rollDice();
+	// const rollValue = rollDice();
 	// Sets text inside cloned dice to equal return of the rollDice
 	// function, random number between 1 and 20
-	diceCopyText.innerText = rollValue;
+	diceCopyText.innerText = rollDice();
 	// Drops the copied dice image and rolled number to the area where
 	// the dice was dropped. Appends as child to parent node 
 	event.target.appendChild(diceCopy);
